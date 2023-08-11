37df1d48cb8442a0b669460e70eed2e72b096575
jxshnpreet <2002jashanpreet@gmail.com>
2023-08-11 15:07:27 +0530

    minor Changes made

--- movieapp.js
+++ movieapp.js
@@ -1,7 +1,7 @@
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=943695765e13b0d3defc9759caa9f210&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=943695765e13b0d3defc9759caa9f210&query=";
-const moiveBox = document.querySelector("#movie-box")
+const movieBox = document.querySelector("#movie-box")
 const getMovies = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
@@ -11,7 +11,7 @@ getMovies(APIURL);
 
 
 const showMovies = (data) => {
-    moiveBox.innerHTML = "";
+    movieBox.innerHTML = "";
     data.results.forEach(
         (result) => {
             const imagePath = result.poster_path === null ? "./assets/missing.jpg" : IMGPATH + result.poster_path;
@@ -31,7 +31,7 @@ const showMovies = (data) => {
                     </p>
                  </div>
             `
-            moiveBox.appendChild(box)
+            movieBox.appendChild(box)
         }
     )
 }
